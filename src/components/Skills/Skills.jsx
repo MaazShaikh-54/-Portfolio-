80bff5801820eb53878715046f9235eafaf127bd
M. MAAZ SHAIKH <109799361+MaazShaikh-54@users.noreply.github.com>
2026-04-17 19:14:10 +0530

    Update skills list and improve component structure

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -4,17 +4,18 @@ import { Circle, Ellipsis, File, Files, Folder, ArrowDownToLine, Parentheses, Br
 const Skills = () => {
 
     const skills = [
-        { key: "Language", value: "JavaScript" },
-        { key: "Framework", value: "React, Node, Express, Tailwind, MUI" },
-        { key: "Database", value: "MySQL, MongoDB" },
-        { key: "Tool", value: "Git" }
+        { key: "Languages", value: "JavaScript, Python" },
+        { key: "Frontend", value: "React.js, Next.js, Tailwind CSS, MUI, Bootstrap" },
+        { key: "Backend", value: "Node.js, Express.js, RESTful APIs" },
+        { key: "Databases", value: "MySQL, MongoDB" },
+        { key: "Tool", value: "Git/GitHub, AWS (EC2, S3), Postman" }
     ];
 
     const totalLines = skills.length + 2;
 
     return (
         <div id='skills' className='bg-[#0e1414] rounded-lg mt-18 mb-1 border-[#3d4917] border-[1.25px] p-[6px] lg:w-[55%] md:w-[70%] sm:w-[90%] w-[95%] mx-auto'>
-        
+            
             <div className='flex justify-between items-center bg-[#1b201e] rounded-t-lg mb-0.5'>
                 <div className='flex mx-2 md:p-3'>
                     <Circle fill='#e64e35' className='w-3 h-3 mx-0.75' />
@@ -40,26 +41,26 @@ const Skills = () => {
                 </div>
             </div>
 
-            <div className='flex text-[14px] font-sans'>
-                
+            <div className='flex font-sans'>
+
                 <div className='text-white font-[JetBrains Mono] text-xs flex flex-col mx-3 my-2 md:text-lg'>
                     {Array.from({ length: totalLines }, (_, i) => (
-                        <p key={i} className='m-2.5'>{i + 1}</p>
+                        <p key={i} className='leading-6 md:leading-8'>{i + 1}</p>
                     ))}
                 </div>
 
-                <div className='md:text-xl'>
-                    <p className='text-white/50 my-1'>{"{"}</p>
+                <div className='md:text-xl my-2'>
+                    <p className='text-white/50 leading-6 md:leading-8'>{"{"}</p>
 
                     {skills.map((item, index) => (
-                        <p key={index} className='my-1 ml-4 leading-6 md:leading-8'>
+                        <p key={index} className='ml-4 leading-6 md:leading-8'>
                             <span className='text-[#78b86a]'>"${item.key}"</span>
                             <span className='text-white font-semibold'>:</span>
                             <span className='font-mono'> "{item.value}"</span>
                         </p>
                     ))}
 
-                    <p className='text-white/50 my-1'>{"}"}</p>
+                    <p className='text-white/50 leading-6 md:leading-8'>{"}"}</p>
                 </div>
 
             </div>
